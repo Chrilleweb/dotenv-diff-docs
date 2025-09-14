2ade5c559d5aa3fbe22b11672e9566000a3a9263
Chrilleweb <christian.munknissen@gmail.com>
2025-09-14 22:05:54 +0200

    workflow

--- svelte.config.js
+++ svelte.config.js
@@ -2,23 +2,23 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    alias: {
-      $components: 'src/components'
-    },
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html'
-    }),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/dotenv-diff' : ''
-    },
-	prerender: {
-    handleHttpError: 'warn'
-  }
-  }
+	preprocess: vitePreprocess(),
+	kit: {
+		alias: {
+			$components: 'src/components'
+		},
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/dotenv-diff-docs' : ''
+		},
+		prerender: {
+			handleHttpError: 'warn'
+		}
+	}
 };
 
 export default config;
